b55a2616948d09e39c100b8168796094d5ece706
Eugene Golubev <emgolubev@gmail.com>
2019-10-19 23:00:19 +0300

    finished cards project

--- deck_test.go
+++ deck_test.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"testing"
+	"os"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Excepted deck length of 52, but got %d", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Excepted the first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d) - 1] != "King of Clubs" {
+		t.Errorf("Excepted the last card of King of Clubs, but got %v", d[len(d) - 1])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(d) != len(loadedDeck) {
+		t.Errorf("Expected the same length of decks, but got %v and %v", len(d), len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
